aa4a58adec926dc6d9484c7af9344480c12155af
ProjectBunny19c <moonlight19c@gmail.com>
2025-01-04 21:51:31 +0800

    On branch V0.1 Your branch is up to date with 'origin/V0.1'.
    Changes to be committed:
    	modified:   C++ Primer/Chapter02_VariablesAndBasicTypes/Exercise02.cpp

--- C++ Primer/Chapter02_VariablesAndBasicTypes/Exercise02.cpp
+++ C++ Primer/Chapter02_VariablesAndBasicTypes/Exercise02.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main(){
     
-
-
-
-
+    int a = 4;
+    const int *const p = &a;
+    int *q = &a;
+    a = 6;
+    // *p = 7;
+    const int b = 5;
+    // int *p2 = &b;
+    int c = b;
 
 
 
